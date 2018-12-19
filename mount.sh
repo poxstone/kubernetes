1d11a8f9c5df050e9879d7327defe9589767538e
poxstone <oscar.ortiz@xertica.com>
2018-12-19 11:51:42 -0500

    update

--- mount.sh
+++ mount.sh
@@ -6,6 +6,7 @@ else
   gcloud beta container clusters create "kuar-cluster" --machine-type "g1-small" --zone "us-east1-b" --preemptible  --enable-autorepair -q;
   gcloud container clusters get-credentials "kuar-cluster";
   kubectl apply -f kuard-pod.yaml;
+  kubectl port-forward kuard 8080:8080;
 fi;
 
 
