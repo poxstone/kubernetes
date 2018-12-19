57ea1a18dbe9874e390a520ff3bfdcda42d73040
developer <developer@instance.vnc>
2018-12-19 17:50:53 +0000

    update

--- mount.sh
+++ mount.sh
@@ -7,6 +7,10 @@ else
   gcloud container clusters get-credentials "kuar-cluster";
   kubectl apply -f kuard-pod.yaml;
   kubectl port-forward kuard 8080:8080;
+
+  #demo
+  kubectl run alpaca-prod --image=gcr.io/kuar-demo/kuard-amd64:1 --replicas=3 --port=8080 --labels="ver=1,app=alpaca,env=prod";
+  kubectl run bandicoot-prod --image=gcr.io/kuar-demo/kuard-amd64:2 --replicas=2 --port=8080 --labels="ver=2,app=bandicoot,env=prod";
 fi;
 
 
