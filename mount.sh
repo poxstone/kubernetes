0ea47ee5f04d9df10f168447481d41228c520bdb
developer <developer@instance.vnc>
2018-12-20 17:19:48 +0000

    add replicaset

--- mount.sh
+++ mount.sh
@@ -11,6 +11,7 @@ else
   #demo
   kubectl run alpaca-prod --image=gcr.io/kuar-demo/kuard-amd64:1 --replicas=3 --port=8080 --labels="ver=1,app=alpaca,env=prod";
   kubectl run bandicoot-prod --image=gcr.io/kuar-demo/kuard-amd64:2 --replicas=2 --port=8080 --labels="ver=2,app=bandicoot,env=prod";
-fi;
+  kubectl expose deployment bandicoot-prod;
 
+fi;
 
