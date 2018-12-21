89052d49d5c34bc0df0ac3a8957e637835bc596b
developer <developer@instance.vnc>
2018-12-21 15:35:55 +0000

    add  job queue

--- mount.sh
+++ mount.sh
@@ -3,10 +3,10 @@
 if [[ "$1" == *"delete"* ]];then
   gcloud beta container clusters delete "kuar-cluster" --zone "us-east1-b" --async -q;
 else
-  gcloud beta container clusters create "kuar-cluster" --machine-type "g1-small" --zone "us-east1-b" --preemptible  --enable-autorepair -q;
+  gcloud beta container clusters create "kuar-cluster" --machine-type "g1-small" --zone "us-east1-b" --disk-size "50" --num-nodes=2 --preemptible  --enable-autorepair -q;
   gcloud container clusters get-credentials "kuar-cluster";
-  kubectl apply -f pod-kuard.yaml;
-  kubectl port-forward kuard 8080:8080;
+  #kubectl apply -f pod-kuard.yaml;
+  #kubectl port-forward kuard 8080:8080;
 
   #demo
   #kubectl run alpaca-prod --image=gcr.io/kuar-demo/kuard-amd64:1 --replicas=3 --port=8080 --labels="ver=1,app=alpaca,env=prod";
